36cb79866d5594035c338c075b23f90f16b91eea
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-17 17:22:21 +0200

    Implement second method of type assertion

--- typeassertions/main.ts
+++ typeassertions/main.ts
@@ -2,4 +2,5 @@
 let message = 'abc';
 //one method of type assertion
 let endsWithC = (<string>message).endsWith('c');
-
+//second method of type assertion
+let alternativeWay = (message as string).endsWith('c');
