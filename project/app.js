14bd6e3467058daa9bc1283282fc006acc3d9443
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-18 18:29:53 +0200

    Introduce arrays

--- project/app.js
+++ project/app.js
@@ -5,6 +5,14 @@
 // better syntax: inferred types
 var person = {
     name: 'Laura',
-    age: 45
+    age: 45,
+    hobbies: ['swimming', 'programming', 'yoga', 'chess']
 };
+// array type assignment
+var favoriteActivities;
+favoriteActivities = ['swimming'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- project/app.ts
+++ project/app.ts
@@ -6,7 +6,16 @@
 // better syntax: inferred types
 const person = {
     name: 'Laura',
-    age: 45
+    age: 45,
+    hobbies: ['swimming', 'programming', 'yoga', 'chess']
 };
 
+// array type assignment
+let favoriteActivities: string[];
+favoriteActivities = ['swimming']
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+}
