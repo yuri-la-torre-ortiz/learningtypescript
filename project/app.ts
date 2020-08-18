a53ec43b62936df667dd65ad80153366adbc1861
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-18 18:50:38 +0200

    Show tuple assignment

--- project/app.ts
+++ project/app.ts
@@ -4,12 +4,18 @@
     age: number
 } = { */
 // better syntax: inferred types
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]; // role is a tuple & requires explicit type assignments
+} = {
     name: 'Laura',
     age: 45,
-    hobbies: ['swimming', 'programming', 'yoga', 'chess']
+    hobbies: ['swimming', 'programming', 'yoga', 'chess'],
+    role: [ 2, 'user']
 };
-
+// person.role.push('admin')  *** push method can alter a tuple
 // array type assignment
 let favoriteActivities: string[];
 favoriteActivities = ['swimming']
