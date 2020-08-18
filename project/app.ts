2871f65be617a636d8fe259361b5baa045987990
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-08-18 18:19:36 +0200

    Examine objects

--- project/app.ts
+++ project/app.ts
@@ -1,10 +1,12 @@
+
 /* const person: {
     name: string;
     age: number
 } = { */
 // better syntax: inferred types
-var person = {
+const person = {
     name: 'Laura',
     age: 45
 };
+
 console.log(person.name);
